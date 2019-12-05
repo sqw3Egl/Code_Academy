94942803ff0b45c175b8f45b544e01178a825c94
Andrew Field <37667576+sqw3Egl@users.noreply.github.com>
2019-12-05 20:39:34 +0000

    Add files via upload
    quick example of and not operators from boolean lesson.

---
+++ operators_test.py
@@ -0,0 +1,13 @@
+# and, not operators example test.
+
+def graduation_reqs(gpa, credits):
+  if (gpa >= 2.0) and (credits >= 120):
+    return "You meet the requirements to graduate!"
+  if (gpa >= 2.0) and (credits < 120):
+    return "You do not have enough credits to graduate."
+  if not (gpa >= 2.0) and (credits >= 120):
+    return "Your GPA is not high enough to graduate."
+  if (gpa < 2.0) and (credits < 120):
+    return "You do not meet either requirement to graduate!"
+  
+print(graduation_reqs(1.5, 90))
