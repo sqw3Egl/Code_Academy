6e3c4e1cefcda19dfdf19ed5dca4a329f43aadfd
Andrew Field <37667576+sqw3Egl@users.noreply.github.com>
2019-12-30 00:49:03 +0000

    Create using_info_within_dictionary.py
    dictionaries lesson 2 - manipulating the information within a dictionary.

---
+++ 8_Dictionaries/using_info_within_dictionary.py
@@ -0,0 +1,95 @@
+#Using information within a dictionary 
+
+## 1. Get a key
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+print(zodiac_elements["earth"])
+print(zodiac_elements["fire"])
+
+## 2.Get an invalid key
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"], "energy": "Not a Zodiac element"}
+
+print(zodiac_elements["energy"])
+
+## 3. Try/Except to Get a Key
+
+caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120, "matcha": 30}
+key_to_check = "matcha"
+try:
+  print(caffeine_level[key_to_check])
+except KeyError:
+  print("Unknown Caffeine Level")
+
+## 4.Safely Get a Key - using .get() method **BEST WAY!!**
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+tc_id = user_ids.get("teraCoder", 100000)
+print(tc_id)
+stack_id = user_ids.get("superStackSmash", 100000)
+print(stack_id)
+
+## 5. Delete a key
+
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+
+print(available_items, health_points)
+
+## 6. Get All Keys - using .keys() method
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+
+print(users, lessons)
+
+## 7. Get all Values - using .values() method
+
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for value in num_exercises.values():
+  total_exercises += value
+  
+print(total_exercises)
+
+## 8. Get all Items (both keys and values) - using .items() method
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Managers": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for key, value in pct_women_in_occupation.items():
+  print('Women make up ' + str(value) + ' percent of ' + key)
+
+## 9. Review
+# Using Dictionaries
+#Review
+#
+#In this lesson, you’ve learned how to go through dictionaries and access keys and values in different ways. Specifically you have seen how to:
+
+#    Use a key to get a value from a dictionary
+#    Check for existence of keys
+#    Find the length of a dictionary
+#    Remove a key: value pair from a dictionary
+#    Iterate through keys and values in dictionaries
+
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your " + key + " is the " + value + " card.")
+
+
+
+
