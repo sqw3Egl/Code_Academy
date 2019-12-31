ad552533a23dc7431060a3057a338b461e68156b
sqw3Egl <sqw3Egl@protonmail.com>
2019-12-31 18:12:33 +0000

    latest

---
+++ 8_Dictionaries/project_scrabble.py
@@ -0,0 +1,10 @@
+##Dictionaries Module - Project 'Scrabble'
+
+
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letters_to_points = {letter:point for letter, point in zip(letters, points)}
+
+print(letters_to_points)
